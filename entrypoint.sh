3ce1498fb782c9dd21c9ab69d12748015ad3ce3e
topcodersonline <43061739+topcodersonline@users.noreply.github.com>
2022-07-15 21:57:48 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,6 @@
 
 export CODE_DIR=${PWD}
 cd $CODE_DIR
-git config --global user.email "you@example.com"
-git config --global user.name "Your Name"
-git init && git add . && git commit -m "init"
 cd $CODE_DIR && checkmate init
 cd $CODE_DIR && checkmate git init
 cd $CODE_DIR && checkmate git analyze
