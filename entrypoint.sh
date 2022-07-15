ddf5d7f4b79eea3ee919876266e9a6ffa99ec506
topcodersonline <43061739+topcodersonline@users.noreply.github.com>
2022-07-15 20:54:40 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 export CODE_DIR=${PWD}
 cd $CODE_DIR
+git init && git add . && git commit -m "init"
 cd $CODE_DIR && checkmate init
 cd $CODE_DIR && checkmate git init
 cd $CODE_DIR && checkmate git analyze
