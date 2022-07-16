138fffaa7a061420280ec9a90dfe282ed910f588
topcodersonline <43061739+topcodersonline@users.noreply.github.com>
2022-07-16 22:32:46 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,3 +6,4 @@ cd $CODE_DIR && checkmate init
 cd $CODE_DIR && checkmate git init
 cd $CODE_DIR && checkmate git analyze
 cd $CODE_DIR && checkmate issues
+git init && git add . && git commit -m "init"
