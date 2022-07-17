7e31b1c01f1af92f6154320e4aa6c7ba12a92b4f
topcodersonline <43061739+topcodersonline@users.noreply.github.com>
2022-07-17 22:13:42 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,3 +9,4 @@ cd $CODE_DIR && checkmate issues
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
 git init && git add . && git commit -m "init"
+git push origin
