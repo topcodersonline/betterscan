4eb7cdfbaf8b973fa5e7b2d7af8f13084a2fdb3c
topcodersonline <43061739+topcodersonline@users.noreply.github.com>
2022-07-17 22:32:32 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,5 @@ cd $CODE_DIR && checkmate git analyze --branch `git rev-parse --abbrev-ref HEAD`
 cd $CODE_DIR && checkmate issues
 git config --global user.email "you@example.com"
 git config --global user.name "Your Name"
-git init && git add . && git commit -m "init"
-git checkout `git rev-parse --abbrev-ref HEAD`
+git add . && git commit -m "state"
 git push origin `git rev-parse --abbrev-ref HEAD`
