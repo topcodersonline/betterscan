0b46b3da8682f130fd0c2a4064dfa4ff15b41a2b
topcodersonline <43061739+topcodersonline@users.noreply.github.com>
2023-06-16 16:23:38 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,4 +7,7 @@ cd $CODE_DIR && checkmate init
 cd $CODE_DIR && checkmate git init
 cd $CODE_DIR && checkmate git analyze --branch `git rev-parse --abbrev-ref HEAD`
 cd $CODE_DIR && checkmate issues html
-mv report.* .checkmate
+git config --global user.email "bot@betterscan.io"
+git config --global user.name "Betterscan.io Bot"
+git add . && git commit -m "state"
+git push origin `git rev-parse --abbrev-ref HEAD`
